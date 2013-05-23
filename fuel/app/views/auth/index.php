e05c72299d430f567de6f850108bd5960c9c7f4d
David Chao <davidchao@fullsail.edu>
2013-05-23 17:27:16 -0500

    Added markdown tags to README. Need to check how it looks.

--- fuel/app/views/auth/index.php
+++ fuel/app/views/auth/index.php
@@ -7,4 +7,4 @@
 <?php echo Html::anchor('auth/create', 'Not a member? Sign up here!',array('class' => 'btn btn-small btn-info')); ?>
 <p></p>
 
-<?php echo Html::anchor('#', 'Forgot your password? Reset it here! NYI',array('class' => 'btn btn-small btn-inverse')); ?>
+<?php echo Html::anchor('#', 'Forgot your password? Reset it here! Coming soon!',array('class' => 'btn btn-small btn-inverse')); ?>
